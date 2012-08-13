dca0c1bcb1affd24b9fa4a20a79706ce13f7c18f
Jeff Powell <powellj@mac.com>
2012-08-13 16:28:40 -0600

    Basic Event Machine echo

---
+++ event.rb
@@ -0,0 +1,13 @@
+require 'eventmachine'
+
+class Echo < EM::Connection
+  def receive_data(data)
+    send_data(data)
+  end
+end
+
+EM.run do
+  EM.start_server("0.0.0.0", 1337, Echo)
+end
+
+# Test via: telnet 127.0.0.1 1337
